f16122c86f2f8de073d6c4e9936bf6ebc3d941f2
Doyoon Kim <asfrom30@gmail.com>
2019-11-14 23:51:32 +0900

    impl mult.hack

--- week4/mult/mult.asm
+++ week4/mult/mult.asm
@@ -1,55 +1,26 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-// Put your code here.
-// @R0
-// D=M
-// @R1
-
 // 루프를 도는 두가지 방법...
 // i를 증가시켜서 R1보다 작게 할 것이냐...
 // R1을 감소시켜서 0보다 작으면 점프... 나는 요거..
 
+M[R2] = 0
+
 (LOOP)
+// end of loop condition
 @R1
 MD=M-1
-
 @END
 D;JLT
+
+@R0
+D=M
+
+@R2 // R2에 저장...
+M=D+M
 
 @LOOP
 0;JMP
 
 (END)
+
 @END
 0;JMP
-
-// for(var i=0; i < 100; i++)
-// @i
-// M=1
-// @sum
-// M=0
-
-// (LOOP)
-// @i
-// D=M
-// @100
-// D=D-A
-// @END
-// D;JGT
-// @i
-// D=M
-// @sum
-// M=D+M
-// @i
-// M=M+1
-// @LOOP
-// 0;JMP
-// (END)
-// @END
-// 0;JMP
