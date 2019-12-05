ee60eb4ea9486aa2c1d863df047ffa87d95ebca5
Doyoon Kim <asfrom30@gmail.com>
2019-12-05 14:12:05 +0900

    make mocha command line and test code organize

--- week6/tests/unit/read-file-line-by-line.spec.js
+++ week6/tests/unit/read-file-line-by-line.spec.js
@@ -1,6 +1,10 @@
 const assert = require("assert");
 const readFileLineByLine = require("../../src/read-file-line-by-line");
 
-readFileLineByLine("sample.txt").then(lines => {
-  assert(lines.length, 5);
+describe("Line Reader", () => {
+  it("read line", () => {
+    readFileLineByLine("sample.txt").then(lines => {
+      assert(lines.length, 5);
+    });
+  });
 });
