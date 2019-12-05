7a2a659e4595c43422a2ba810ed705bd96dcceb1
Doyoon Kim <asfrom30@gmail.com>
2019-12-05 14:02:23 +0900

    impl sanitize() and commandType()

--- week6/main.js
+++ week6/main.js
@@ -2,11 +2,31 @@ const readFileLineByLine = require("./src/read-file-line-by-line");
 
 const targetDir = "./tests/e2e/pong/PongL.asm";
 readFileLineByLine(targetDir).then(lines => {
-  const bits = lines.map(line => noSymbolAssembler(line));
-  // reporter
-  console.log(bits);
+  const res = [];
+
+  lines.forEach(line => {
+    const str = sanitize(line);
+    if (!str) return;
+
+    const cmd = str;
+    res.push(noSymbolAssembler(cmd));
+  });
+
+  console.log(res); // reporter
 });
 
 function noSymbolAssembler(line) {
+  return line + " :: " + commandType(line);
+}
+
+function commandType(cmd) {
+  if (cmd.includes("@")) return "A_COMMAND";
+  else if (cmd.includes(";") || cmd.includes("=")) return "C_COMMAND";
+  else return "L_COMMAND";
+}
+
+function sanitize(line) {
+  if (line.includes("//")) return false;
+
   return line;
 }
