6fa1e3be41223e3964ec56fe2edc27bc4276611f
Doyoon Kim <asfrom30@gmail.com>
2019-12-05 13:30:30 +0900

    separte test and codE

--- week6/main.js
+++
@@ -1,6 +0,0 @@
-const assert = require("assert");
-const readFileLineByLine = require("./src/read-file-line-by-line");
-
-readFileLineByLine("sample.txt").then(lines => {
-  assert(lines.length, 5);
-});
